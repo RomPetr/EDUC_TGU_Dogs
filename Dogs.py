95b86a22ed831d7c39cb73afdd5fa18351ac5cf5
Rompetr78 <rompetr@yahoo.com>
2024-11-07 12:02:07 +0800

    Создан файл Dogs.py с окном window модуля Tkinter

---
+++ Dogs.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+
+
+window = Tk()
+window.title("Картинки с собачками")
+window.geometry("360x420")
+
+
+label = Label()
+label.pack(pady=10)
