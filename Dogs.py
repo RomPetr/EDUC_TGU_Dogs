570f8eac2df2d1b138ba1fc8f4c02b25f502deab
Rompetr78 <rompetr@yahoo.com>
2024-11-07 12:04:19 +0800

    Добавили метку label и кнопку button

--- Dogs.py
+++ Dogs.py
@@ -4,6 +4,8 @@ from PIL import Image, ImageTk
 from io import BytesIO
 
 
+def
+
 window = Tk()
 window.title("Картинки с собачками")
 window.geometry("360x420")
@@ -11,3 +13,8 @@ window.geometry("360x420")
 
 label = Label()
 label.pack(pady=10)
+
+button = Button(text="Загрузить изображение", command=show_image)
+button.pack(pady=10)
+
+window.mainloop()
